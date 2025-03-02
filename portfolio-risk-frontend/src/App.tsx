40aeaadb2260ab6b874b3950621982fc5ced3f15
Roger Li <rel7817@nyu.edu>
2025-03-02 00:23:29 -0500

    commit

---
+++ portfolio-risk-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainContainer from './components/MainContainer';
+
+function App() {
+  return (
+    // <div className="App">
+    // </div>
+    <div>
+      <MainContainer/>
+    </div>
+  );
+}
+
+export default App;
