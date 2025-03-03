a455d0b10c94aa1d3d252ab565871f9b62cedefd
Roger Li <rel7817@nyu.edu>
2025-03-02 21:16:36 -0500

    idk'

--- portfolio-risk-frontend/src/interfaces/riskStats.ts
+++ portfolio-risk-frontend/src/interfaces/riskStats.ts
@@ -5,7 +5,8 @@ export interface RiskStats {
     sp500_volatility: number;
     sp500_VaR: number;
     sp500_ES: number;
-    logreturn_ts: Record<string, number>; // Keys are date strings, values are numbers
+    logreturn_ts: Record<string, number>;
     portfolio_ts: Record<string, number>;
     scaled_sp500_ts: Record<string, number>;
 }
+
